46658d5edd9c787a6823ad2bc606ea954a53c59e
NithinKonda <nithinkonda142@gmail.com>
2024-08-29 23:20:36 +0530

    add some effects

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 "use client"
 import { useRouter } from 'next/navigation';
+import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 
 export default function Home() {
   const router = useRouter();
+  const words = `Group ChatGPT with Gemini`
 
   const handleLoginRedirect = () => {
     router.push('/login'); // Redirect to /login page
@@ -10,7 +12,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white">
-      <h1 className="text-4xl font-bold mb-8">Group ChatGPT with Gemini</h1>
+      <h1 className="text-4xl font-bold mb-8 text-white">
+        <TextGenerateEffect words={words} />
+      </h1>
       <button
         onClick={handleLoginRedirect}
         className="mt-8 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"

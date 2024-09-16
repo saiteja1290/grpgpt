2509de3e404945914ed501c51d73d0c7c4c00026
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-09-16 20:11:22 +0530

    tried changing theme but did not work

--- app/page.js
+++ app/page.js
@@ -1,24 +1,4 @@
-// "use client"
-// import { useRouter } from 'next/navigation';
-// import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 
-// export default function Home() {
-//   const router = useRouter();
-//   const words = `Group ChatGPT with Gemini`
-
-// const handleLoginRedirect = () => {
-//   router.push('/login'); // Redirect to /login page
-// };
-
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white">
-//       <h1 className="text-4xl font-bold mb-8 text-white">
-//         <TextGenerateEffect words={words} />
-//       </h1>
-
-//     </main>
-//   );
-// }
 "use client";
 import React from "react";
 import { useRouter } from 'next/navigation';

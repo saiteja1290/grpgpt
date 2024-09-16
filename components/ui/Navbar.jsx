093d9d97aec18d36d16f5eeed14f12b9d12e7dd5
NithinKonda <nithinkonda142@gmail.com>
2024-09-17 00:38:33 +0530

    fix navbar

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -3,8 +3,8 @@ const Navbar = () => {
       <nav className="fixed top-0 left-0 right-0 bg-black p-5 z-10 m-3">
         <ul className="flex space-x-4 text-white justify-end">
           <li><a href="/" className="hover:underline">Home</a></li>
-          <li><a href="/login" className="hover:underline">Signup</a></li>
-          <li><a href="/signup" className="hover:underline">LogIn</a></li>
+          <li><a href="/signup" className="hover:underline">Signup</a></li>
+          <li><a href="/login" className="hover:underline">LogIn</a></li>
         </ul>
       </nav>
     );
